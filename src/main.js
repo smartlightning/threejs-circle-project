4bd539a9353e0bdf10cbc2dc7fcaeaf381f0f959
chrismcqueen <chris.mcqueen@bergfreunde.de>
2024-12-05 09:02:24 +0100

    Add rotation function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import './style.css'
 import * as THREE from 'three'
 
 // Scene
 const scene = new THREE.Scene()
+
+// Canvas
+const canvas = document.querySelector('.webgl')
+// sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
 
 // Create a sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64)
@@ -16,19 +26,37 @@ pointLight.position.set(0, 10, 10)
 scene.add(pointLight)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 20
 scene.add(camera)
 
+
 // Renderer
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('.webgl') })
-renderer.setSize(window.innerWidth, window.innerHeight)
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.render(scene, camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 // Resize handler (optional)
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  // Update Camera
+  camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // Render sizes
+  renderer.setSize(sizes.width, sizes.height)
 })
+
+// Rerenders scene when the window is resized
+const loop = () => {
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
